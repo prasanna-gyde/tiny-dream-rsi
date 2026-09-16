5676c5d222ee150405204aed498b318dc1cbf515
Prasanna Vaidya <47359330+prasanna-gyde@users.noreply.github.com>
2026-09-16 22:36:54 +0530

    Add the discovery-tree favicon
    Add an SVG favicon matching the new brand mark and serve it with the correct MIME type.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const TYPES = {
   '.js': 'text/javascript; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
   '.json': 'application/json; charset=utf-8',
+  '.svg': 'image/svg+xml',
 };
 
 const server = http.createServer((req, res) => {
